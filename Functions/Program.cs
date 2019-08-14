94bc2ee32c7afd5d257e480686e0256e8046f34a
Jacob Firsdon <firsdonj@gmail.com>
2019-08-14 10:59:52 -0700

    Finished Exersise

--- Functions/Program.cs
+++ Functions/Program.cs
@@ -8,8 +8,33 @@ namespace Functions
 {
     class Program
     {
+
         static void Main(string[] args)
         {
+            Game gm = new Game();
+            gm.score = 100;
+            gm.start();
+        }
+    }
+    class Game
+    {
+        public int score = 0;
+        public void start()
+        {
+            printScore(3);
+            printScore(14);
+            printScore(1512);
+            Console.ReadKey();
+
+        }
+        public int addToScore(int add)
+        {
+            score += add;
+            return score;
+        }
+        public void printScore(int add)
+        {
+            Console.WriteLine("Score: "+addToScore(add));
         }
     }
 }
